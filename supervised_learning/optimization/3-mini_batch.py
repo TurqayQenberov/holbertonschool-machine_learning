cc93a2f961c3c525a0de536e53d78ca433a52e9e
TurqayQenberov <67190795+TurqayQenberov@users.noreply.github.com>
2026-09-13 19:58:44 +0400

    Add files via upload

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
-"""Module to create mini-batches for neural network training."""
-
+# -*- coding: utf-8 -*-
+"""
+Creates mini-batches for mini-batch gradient descent
+"""
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def create_mini_batches(X, Y, batch_size):
-    """Creates mini-batches from input data and labels.
+    """
+    Creates mini-batches to be used for training a neural network using
+    mini-batch gradient descent
 
-    Args:
-        X: numpy.ndarray of shape (m, nx) representing input data
-        Y: numpy.ndarray of shape (m, ny) representing labels
-        batch_size: number of data points in a batch
+    Arguments:
+     - X: numpy.ndarray of shape (m, nx) representing input data
+        * m is the number of data points
+        * nx is the number of features in X
+     - Y: numpy.ndarray of shape (m, ny) representing the labels
+        * m is the same number of data points as in X
+        * ny is the number of classes for classification tasks
+     - batch_size: the number of data points in a batch
 
     Returns:
-        list of tuples (X_batch, Y_batch)
+    list of mini-batches containing tuples (X_batch, Y_batch)
     """
     X_shuffled, Y_shuffled = shuffle_data(X, Y)
+
     m = X.shape[0]
     mini_batches = []
 
